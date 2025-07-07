8292236beb9adb3de7603ecffc0cab846e3912a9
quick175 <ahmedalshawafi75@gmail.com>
2025-07-08 01:26:44 +0300

    Update BMI.py

--- BMI.py
+++ BMI.py
@@ -26,7 +26,7 @@ def unity():
 		weight = float(weight)
 	elif wcalc == "lb":
 		weight = float(weight) * 0.45359237
-	def loading():
+def loading():
 	symbols = ['-', '/', '|', '\\']
 	start_time = time.time()
 	i = 0
@@ -35,26 +35,26 @@ def unity():
 		print('\r\033[K%s Calculating...' % symbols[i], flush=True, end='')
 		time.sleep(0.1)
 	print('\r\033[K', end='')
-	return
+	return()
 	
-	listofunits = ["m" ,"ft", "cm"]
-    if unit in listofunits:
-        x = listofunits.index(unit)
-        print(f"You selected unit: {listofunits[x]}")
-    else:
-        print(f"{unit} is invalid")
-        exit()
+listofunits = ["m" ,"ft", "cm"]
+if unit in listofunits:
+     	x = listofunits.index(unit)
+    	print(f"You selected unit: {listofunits[x]}")
+else:
+    print(f"{unit} is invalid")
+    exit()
 	
-	if unit="cm":
-		height= height(float) *100
-	elif unit=="m":
-		height=float(height)
-	elif unit=="ft":
-		height=float(height) * 0.3048
-	k=height*height
-	bmi = weight / k
-	loading()
-	print("Your BMI is: ", bmi)
+if unit="cm":
+	height= height(float) *100
+elif unit=="m":
+	height=float(height)
+elif unit=="ft":
+	height=float(height) * 0.3048
+k=height*height
+bmi = weight / k
+loading()
+print("Your BMI is: ", bmi)
 	
 	
 
